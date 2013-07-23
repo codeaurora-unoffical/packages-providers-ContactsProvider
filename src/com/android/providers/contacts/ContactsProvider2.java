5eaa59cade12474a1d34e714af4e5961ddec5800
kaiyiz <kaiyiz@codeaurora.org>
2013-07-23 12:14:31 +0800

    Contacts: Fix the issue of contacts with same values only display one
    The default aggregation mode was RawContacts.AGGREGATION_MODE_DEFAULT.
    At this condition, raw contacts items were aggregated immediately when
    insert operation was completed. As a result, items with same valuses
    would have same contact_id. So, contacts list only display one vcard
    file contacts after imported one vcard file sereral times.
    
    Change aggregation mode from RawContacts.AGGREGATION_MODE_DEFAULT to
    RawContacts.AGGREGATION_MODE_SUSPENDED. This will not trigger aggregate
    when insert operation was finished and items with same values will have
    different id. So, contacts in one vcard file can display several times
    after change codes.
    
    Change-Id: I6f7a044fb4fdf93dd41c71d4f382ef24d0b18f11
    CRs-Fixed: 509642

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -2627,7 +2627,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
         if (rawContactId != -1) {
             insertSimPhoto(accountWithDataSet, rawContactId);
         }
-        int aggregationMode = RawContacts.AGGREGATION_MODE_DEFAULT;
+        int aggregationMode = RawContacts.AGGREGATION_MODE_SUSPENDED;
         if (mValues.containsKey(RawContacts.AGGREGATION_MODE)) {
             aggregationMode = mValues.getAsInteger(RawContacts.AGGREGATION_MODE);
         }
